925b12d7a9ab0f969ee57dd1a7c6809d22948721
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-29 14:38:33 +0100

    added logback config

--- src/main/java/pl/zajavka/Calculator.java
+++ src/main/java/pl/zajavka/Calculator.java
@@ -1,7 +1,14 @@
 package pl.zajavka;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Calculator {
     public int add(int left, int right) {
+        log.debug("DEBUG message");
+        log.info("INFO message");
+        log.warn("WARN message");
+        log.error("ERROR message");
         return left + right;
     }
     public int subtract(int left, int right) {
