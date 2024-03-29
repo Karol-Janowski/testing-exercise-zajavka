6388ed7cf4a37682567240c5fffdf94249f694bc
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-29 14:10:01 +0100

    added example of parametrized test method

--- src/test/java/pl/zajavka/CalculatorTest.java
+++ src/test/java/pl/zajavka/CalculatorTest.java
@@ -4,14 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CalculatorTest {
 
     private Calculator calculator;
+    Throwable exception =
+            Assertions.assertThrows(NumberFormatException.class, () -> Integer.parseInt("MyInput"));
+
     @BeforeEach
     void beforeEach() {
-       calculator = new Calculator();
+        calculator = new Calculator();
     }
+
     @Test
     @DisplayName("Simple calculator addition should work correctly")
     void addTest() {
@@ -21,7 +27,7 @@ class CalculatorTest {
         Integer expected = 15;
 
         //when
-        Integer result = calculator.add(left ,right);
+        Integer result = calculator.add(left, right);
 
         //then
         Assertions.assertEquals(expected, result);
@@ -35,7 +41,7 @@ class CalculatorTest {
         Integer expected = 5;
 
         //when
-        Integer result = calculator.subtract(left ,right);
+        Integer result = calculator.subtract(left, right);
 
         //then
         Assertions.assertEquals(expected, result);
@@ -49,7 +55,7 @@ class CalculatorTest {
         Integer expected = 50;
 
         //when
-        Integer result = calculator.multiply(left ,right);
+        Integer result = calculator.multiply(left, right);
 
         //then
         Assertions.assertEquals(expected, result);
@@ -63,7 +69,7 @@ class CalculatorTest {
         Integer expected = 2;
 
         //when
-        Integer result = calculator.divide(left ,right);
+        Integer result = calculator.divide(left, right);
 
         //then
         Assertions.assertEquals(expected, result);
@@ -88,6 +94,29 @@ class CalculatorTest {
                 () -> Assertions.assertEquals("test1", "test2", () -> createMessage(2)),
                 () -> Assertions.assertEquals("test1", "test4", () -> createMessage(3))
         );
+    }
+
+    @Test
+    void exceptionTest() {
+        Assertions.assertEquals("For input string: \"MyInput\"", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "testData")
+    void testParametrizedAdding(int[] testData) {
+// given
+        int left = testData[0];
+        int right = testData[1];
+        Integer expected = testData[2];
+// when
+        Integer result = calculator.add(left, right);
+// then
+        Assertions.assertEquals(expected, result);
+    }
+
+    @SuppressWarnings("unused")
+    public static int[][] testData() {
+        return new int[][]{{1, 1, 2}, {2, 3, 5}, {9, 8, 17}, {2, 19, 21}};
     }
 
 
